1715e1350cac6f0b37f2e730ee8b5e264c0b8934
pinguin230 <tesliamain.com>
2024-02-29 12:15:43 +0200

    adding core for test get request

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
-import { userRouter } from './src/controllers/user.controller'
+import { userRouter } from './src/controllers/user.controller';
 
 const app = express();
 const port = 5000;
 
+app.use(cors({
+  origin: 'http://localhost:3001'
+}));
+
 app.use(bodyParser.json());
+
 app.use('/api', userRouter);
 
 app.listen(port, async () => {
