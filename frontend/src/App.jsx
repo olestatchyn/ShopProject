a3f5c2924670da0461bb4041fa91bf8b81fa7b38
pinguin230 <tesliamain.com>
2024-02-29 11:54:27 +0200

    adding get request

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import axios from "axios"
+
+function App() {
+
+	// const [data, setData] = useState([{}]);
+
+	const userData = {
+		name: "Mykola",
+		password: "1234"
+	};
+
+	const fetchData = async () => {
+		const responce = await axios("https://jsonplaceholder.typicode.com/todos/1")
+		console.log(responce.data)
+	}
+
+	const fetchDataPost = async () => {
+		const responce = await axios.post('http://example.com/api/users', userData)
+		console.log(responce.data)
+	}
+
+  return (
+    <div>
+      {/* <h1>Hello world</h1> */}
+			<div>
+				<input style={{marginTop: 10}} type="text" />
+			</div>
+			<div>
+				<input style={{marginTop: 10}} type="text" />
+			</div>
+			<div>
+				<button  style={{marginTop: 10}} onClick={fetchData}> Get Data</button>
+			</div>
+			<div>
+				<button  style={{marginTop: 10}} onClick={fetchDataPost}> Post Data</button>
+			</div>
+    </div>
+  );
+}
+
+export default App;
