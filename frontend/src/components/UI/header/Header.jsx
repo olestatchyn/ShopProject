7a74be705ff66c656ada67f33232672196a6f545
pinguin230 <tesliamain@gmail.com>
2024-03-09 20:38:06 +0200

    adding style for header

--- frontend/src/components/UI/header/Header.jsx
+++ frontend/src/components/UI/header/Header.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import cl from './Header.module.css'
 import main from "./../../../style/style.module.css"
 import MyNavLink from '../nav-bar/MyNavLink';
+import { useAuth } from '../../../utils/AuthContext';
+import MyButton from '../button/MyButton';
 
 const Header = () => {
+
+	const { isAuthenticated, userRole } = useAuth();
+
 	return (
 		<div className={cl.header}>
 			<div className={main.container}>
 				<div className={cl.header__row}>
+					<div className={cl.header__col}>
+						<div>Тут фотка</div>
+					</div>
 					<div className={cl.header__col}>
 						<div className={cl.header__pages}>
 							<div className={cl.header__pages__item}>
@@ -21,11 +29,14 @@ const Header = () => {
 							</div>
 						</div>
 					</div>
+					
 					<div className={cl.header__col}>
-
+						<div>тут інфа про годину</div>
+						<div>тут інфа про номер</div>
 					</div>
 					<div className={cl.header__col}>
-
+						<div><MyButton>Log In</MyButton></div>
+						<div><MyButton>Sing Up</MyButton></div>
 					</div>
 				</div>
 			</div>
