29fadef3869e8bb0a5eb4dc314b64d57ad6f5270
pinguin230 <tesliamain@gmail.com>
2024-03-09 21:22:09 +0200

    adding style for header

--- frontend/src/components/UI/header/Header.jsx
+++ frontend/src/components/UI/header/Header.jsx
@@ -1,47 +1,55 @@
 import React from 'react';
-import cl from './Header.module.css'
-import main from "./../../../style/style.module.css"
-import MyNavLink from '../nav-bar/MyNavLink';
-import { useAuth } from '../../../utils/AuthContext';
+import cl from './Header.module.css';
+import logo from './../../../sourse/Logo.svg';
+import MyNavLink from './../nav-bar/MyNavLink';
 import MyButton from '../button/MyButton';
 
 const Header = () => {
-
-	const { isAuthenticated, userRole } = useAuth();
-
 	return (
 		<div className={cl.header}>
-			<div className={main.container}>
+			<div className={cl.header__container}>
 				<div className={cl.header__row}>
 					<div className={cl.header__col}>
-						<div>Тут фотка</div>
+						<div className={cl.header__logo}>
+							<MyNavLink className="myNavLink" to="/">
+								<img src={logo} alt="Logo"/>
+							</MyNavLink>
+						</div>
 					</div>
 					<div className={cl.header__col}>
 						<div className={cl.header__pages}>
-							<div className={cl.header__pages__item}>
-								<MyNavLink to="shares">Акції</MyNavLink>
-							</div>
-							<div className={cl.header__pages__item}>
-								<MyNavLink to="payment_and_delivery">Доставка і оплата</MyNavLink>
-							</div>
-							<div className={cl.header__pages__item}>
-								<MyNavLink to="about"> Про нас</MyNavLink>
-							</div>
+							<MyNavLink className={cl.myNavLink} to="shares">
+								<div className={`${cl.header__pages__item} ${cl.active__page}`}>
+									Акції
+								</div>
+							</MyNavLink>
+							<MyNavLink className={cl.myNavLink} to="payment_and_delivery">
+								<div className={cl.header__pages__item}>
+									Доставка і оплата
+								</div>
+							</MyNavLink>
+							<MyNavLink className={cl.myNavLink} to="about">
+								<div className={cl.header__pages__item}>
+									Про нас
+								</div>
+							</MyNavLink>
 						</div>
 					</div>
-					
 					<div className={cl.header__col}>
-						<div>тут інфа про годину</div>
-						<div>тут інфа про номер</div>
-					</div>
-					<div className={cl.header__col}>
-						<div><MyButton>Log In</MyButton></div>
-						<div><MyButton>Sing Up</MyButton></div>
+						<div className={cl.header__buttons}>
+							<div className={cl.header__LogIn}>
+								<MyButton className={cl.button}>Log In</MyButton>
+							</div>
+							<div className={cl.header__SignUp}>
+								<MyButton className={cl.button}>Sign Up</MyButton>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	);
 }
+
 
 export default Header;
