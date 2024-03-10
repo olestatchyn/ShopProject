344ac043bdc138d297d5e9866041a8907db39d80
DonatoMoon <prvt.wrk.ndrsvchnk@gmail.com>
2024-03-11 00:39:19 +0200

    adding about us first frame

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
-
+import cl from './About.module.css';
+import main from './../../style/style.module.css';
+import facebook from './../../source/about us/facebook.svg';
+import instagram from './../../source/about us/instagram.svg';
+import gmail from './../../source/about us/email.svg';
 const About = () => {
 	return (
-		<div>
-			About page
+		<div className={cl.about}>
+
+			
+			<div className={cl.about__pizza}>
+				<div className={cl.about__contacts}>
+					<div className={cl.about__column}>
+						<div className={cl.about__contacts__img}>
+							<img src={facebook} alt=""/>
+						</div>
+						<div className={cl.about__contacts__title}>
+							Facebook
+						</div>
+						<div className={cl.about__contacts__text}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis
+						</div>
+					</div>
+					<div className={cl.about__column}>
+						<div className={cl.about__contacts__img}>
+							<img src={instagram} alt=""/>
+						</div>
+						<div className={cl.about__contacts__title}>
+							Instagram
+						</div>
+						<div className={cl.about__contacts__text}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis
+						</div>
+					</div>
+					<div className={cl.about__column}>
+						<div className={cl.about__contacts__img}>
+							<img src={gmail} alt=""/>
+						</div>
+						<div className={cl.about__contacts__title}>
+							Gmail
+						</div>
+						<div className={cl.about__contacts__text}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={cl.about__space}>
+
+			</div>
+
+
+			<div className={main.container}>
+
+
+
+
+			</div>
 		</div>
 	);
 }
