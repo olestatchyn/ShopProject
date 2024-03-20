975dc2586d5ffe566dfb0ae7eed8ea02a9b76ea7
Mykhailo <matsyshynmykhailo@gmail.com>
2024-03-20 20:11:47 +0200

    add scroll to top button

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import main from './../../style/style.module.css'
-import cl from './Home.module.scss'
-import CreateCustomPizza from '../../components/create custom pizza/CreateCustomPizza';
+import React from "react";
+import main from "./../../style/style.module.css";
+import cl from "./Home.module.scss";
+import CreateCustomPizza from "../../components/create custom pizza/CreateCustomPizza";
 import FeedbackForm from "../../components/feedback form/FeedbackForm";
 
-import Menu from '../../components/menu/Menu';
+import Menu from "../../components/menu/Menu";
 
 import CarouselComponent from "../../components/carousel/Carousel";
-import TextLogo from '../../components/UI/text-logo/TextLogo';
-
+import TextLogo from "../../components/UI/text-logo/TextLogo";
+import ScrollToTopButton from "../../components/UI/ScrollToTopButton/ScrollToTopButton";
 
 const Home = () => {
-  
+  return (
+    <div className={cl.home}>
+      <div className={main.container}>
+        <Menu />
+        <CreateCustomPizza />
+      </div>
+      <div className={cl.home__carousel}>
+        <CarouselComponent />
+        <TextLogo />
+      </div>
+      <FeedbackForm />
 
-	return (
-		<div className={cl.home}>
-
-			<div className={main.container}>
-				<Menu />
-				<CreateCustomPizza />
-			</div>
-			<div className={cl.home__carousel}>
-				<CarouselComponent />
-				<TextLogo />
-			</div>
-			<FeedbackForm />
-		</div>
-	);
-}
+      <ScrollToTopButton />
+    </div>
+  );
+};
 
 export default Home;
