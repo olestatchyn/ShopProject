2961ee6874a8f345422fa12ad8a14f0a5d3e6c0e
pinguin230 <tesliamain@gmail.com>
2024-03-26 16:30:40 +0200

    update rout for picking random pizza and delete incorect folder

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,11 +3,11 @@ import main from "./../../style/style.module.css";
 import cl from "./Home.module.scss";
 import CreateCustomPizza from "../../components/create custom pizza/CreateCustomPizza";
 import FeedbackForm from "../../components/feedback form/FeedbackForm";
-import RandomPizza from "../../components/RandomPizza/RandomPizza";
 import Menu from "../../components/menu/Menu";
 
 import CarouselComponent from "../../components/carousel/Carousel";
 import TextLogo from "../../components/UI/text-logo/TextLogo";
+import RandomPizza from "../../components/random pizza/RandomPizza";
 
 const Home = () => {
   return (
